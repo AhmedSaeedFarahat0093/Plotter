24111cb1ee7188974a9ce7433322bd5886321376
Abd ElHamid <ahmed.abdelhamid5@vodafone.com>
2021-09-22 15:55:47 +0200

    Adding A header component and use it inside app component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { homeLabel, title } from '../defines';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  title: string;
+  home: string;
+  constructor() { }
+
+  ngOnInit() {
+  this.title = title;
+  this.home = homeLabel;
+  }
+
+}
